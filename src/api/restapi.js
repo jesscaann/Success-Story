1e636c5f87519ee8851b563f272358a8d7e7bf6b
Caleb <c.kopp.professional@gmail.com>
2019-04-19 19:45:01 -0500

    Create restapi.js
    - This is to be imported into any file that will use the RESTapi.
    - all CRUD operations are to be implemented seperately and atomically here, so as to keep the code within the components purely about React

---
+++ src/api/restapi.js
@@ -0,0 +1,71 @@
+import api from "./axios-rest-connection";
+
+class axiosHandler {
+  static getClients = async () => {
+    var clients = {};
+    await api.get("/clients/").then(res => {
+      clients = res.data;
+
+      /**
+       * Manipulate data to have a keys of
+       * key: text: value:
+       * that all have the data from clientName
+       */
+      clients.forEach(client => {
+        var name = client.clientName;
+
+        /**
+         * Takes in each object from the axios response and manipulates its data
+         *
+         * Since the dropdown menu requires a list of objects with the three keys
+         * - key
+         * - text
+         * - value
+         * we set all of these to be the clientName from the client object
+         */
+        delete client.clientName;
+        delete client.caseStudies;
+        client.key = name;
+        client.text = name;
+        client.value = name;
+      });
+    });
+
+    return clients;
+  };
+  static getEngagementNames = async () => {
+    /**
+     * Eventually, Sogeti will have to store potential engagement names themselves.
+     * So this is in preparation for that. For the moment though, we have
+     * dummy data.
+     */
+    return [
+      {
+        id: 1,
+        key: "Discov&CloudMigrRead",
+        text: "Discovery & Cloud Migration Readiness",
+        value: "Discovery & Cloud Migration Readiness "
+      },
+      {
+        id: 2,
+        key: "EngageName2",
+        text: "Engagement Name 2",
+        value: "Engagement Name 2 "
+      },
+      {
+        id: 3,
+        key: "EngageName3",
+        text: "Engagement Name 3",
+        value: "Engagement Name 3 "
+      },
+      {
+        id: 4,
+        key: "EngageName4",
+        text: "Engagement Name 4",
+        value: "Engagement Name 4 "
+      }
+    ];
+  };
+}
+
+export default axiosHandler;
